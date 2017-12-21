ec13fb1fdef5ac27e98e865fc3f9f0fe3eb493a0
Aldi Fahrezi <aldi.fahrezi@gmail.com>
2017-12-21 12:04:18 +0000

    Add webservice for object_detection

--- object-detection/deep_learning_object_detection.py
+++ object-detection/deep_learning_object_detection.py
@@ -6,6 +6,8 @@
 import numpy as np
 import argparse
 import cv2
+import logging
+import json
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -30,7 +32,7 @@ CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
 COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
 # load our serialized model from disk
-print("[INFO] loading model...")
+logging.debug("[INFO] loading model...")
 net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
 # load the input image and construct an input blob for the image
@@ -43,9 +45,10 @@ blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843, (300, 300)
 
 # pass the blob through the network and obtain the detections and
 # predictions
-print("[INFO] computing object detections...")
+logging.debug("[INFO] computing object detections...")
 net.setInput(blob)
 detections = net.forward()
+result = []
 
 # loop over the detections
 for i in np.arange(0, detections.shape[2]):
@@ -65,7 +68,8 @@ for i in np.arange(0, detections.shape[2]):
 
 		# display the prediction
 		label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
-		print("[INFO] {}".format(label))
+		result.append({CLASSES[idx]: float(confidence)})
+		logging.debug("[INFO] {}".format(label))
 		cv2.rectangle(image, (startX, startY), (endX, endY),
 			COLORS[idx], 2)
 		y = startY - 15 if startY - 15 > 15 else startY + 15
@@ -74,5 +78,6 @@ for i in np.arange(0, detections.shape[2]):
 
 # show the output image
 # cv2.imshow("Output", image)
+print(json.dumps(result))
 cv2.imwrite(args["output"], image)
 cv2.waitKey(0)
